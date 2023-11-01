a4c1728ca5c1f7cfac43477295643182ed98b7dc
rahul231120 <rahul231120@gmail.com>
2023-11-01 15:54:33 +0530

    finished project

---
+++ articleblog/articleblog/urls.py
@@ -0,0 +1,21 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from .import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+from articles import views as av
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('articles/',include('articles.urls')),
+    path('accounts/',include('accounts.urls')),
+    path('about/',views.about),
+    path('',av.articles_list,name ="home" )
+    
+]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
